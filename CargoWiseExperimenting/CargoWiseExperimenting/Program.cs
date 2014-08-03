d2ec8cfb389b64eab26f567aee39ee7d657e7dcc
Igor Malin <malin.igor@gmail.com>
2014-08-03 13:11:54 +0300

    Experimenting

---
+++ CargoWiseExperimenting/CargoWiseExperimenting/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CargoWiseExperimenting
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var pricer = new SomeClass("AUX");
+            pricer.PriceChanged += PricerOnPriceChanged;
+            pricer.Price = 10;
+        }
+
+        private static decimal PricerOnPriceChanged(decimal oldPrice, decimal newPrice)
+        {
+            return oldPrice - newPrice;
+        }
+    }
+}
